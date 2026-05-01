8ef11faff54002c173a2e9e9e1e40a628a134698
Nilay Saxena <nilaysaxena94@gmail.com>
2026-05-01 14:10:41 +0000

    Add monitoring resource group to Terraform configuration

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "azurerm_resource_group" "core" {
     environment : var.environment
   }
 }
+
+resource "azurerm_resource_group" "monitoring" {
+  name     = "${local.namespace_prefix}rg-monitoring-${var.environment}-${var.location}"
+  location = var.location
+
+  tags = {
+    environment : var.environment
+  }
+}
